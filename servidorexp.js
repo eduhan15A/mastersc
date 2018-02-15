0cf53b9b825adb00a8168cdf27d9aa1965d38799
eduhan <edwardoluhan@gmail.com>
2018-02-14 21:29:55 -0600

    Se crearon vistas con ejs y se estructuro dinámicamente el pase de vistas en una sola página page.ejs

--- servidorexp.js
+++ servidorexp.js
@@ -2,15 +2,29 @@ const expressLib = require ("express");
 const port = 3001;
 var app = expressLib();
 var path    = require("path");
-app.use(expressLib.static("public"));
+const plantilla = require("ejs");
+
+//app.use(expressLib.static("public"));
 //app.use(app.router);
 //app.use(expressLib.static(path.join(__dirname, 'public')));
 
+app.use(expressLib.static('public'))
+app.use(expressLib.static('Views'))
+app.set("view engine","ejs");
 
-app.get('/', (req,response)=>{
+
+app.get('*', (req,response)=>{
 //response.send('Hola Chiquitín');
- response.sendFile(path.join(__dirname+'/index.html'));
+ //response.sendFile(path.join(__dirname+'/index.html'));
+//response.sendFile(path.join(__dirname+'../views/index.ejs'));
+
+		response.render("page", { data: req.url });
+		//response.render("contacto")
+
 });
+//app.get('/contacto', (req,response)=>{
+	//	response.render("contacto")
+//});
 
 
 console.log(`Running at Port ${port}`);

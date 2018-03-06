90db860661e4f815fc934b31c9e0b41ae2dc16c3
Eduardo Lujan <eduardolujan@Eduardos-Air.lan>
2018-03-05 21:52:03 -0600

    Se cambia el puerto 3000 a 80 para que funcione en heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const protocol = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 80;
 
-const server =  protocol.createServer(( req, response) => {
-		response.statusCode = 200;
-		response.setHeader('Content-Type','application/json;charset=UTF-8');
-		response.end('{"Servidor":"Chiquitín"}');
+const server = protocol.createServer((req, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json;charset=UTF-8');
+    response.end('{"Servidor":"Chiquitín"}');
 
 });
 
-server.listen(port, hostname,() => {
-	console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+server.listen(port, hostname, () => {
+    console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 });
